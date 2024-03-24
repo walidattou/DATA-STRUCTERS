4637d84631a37e599a42b1569b63fc2dd09f9437
leto_big_boy <walidattou123A@gmail.com>
2024-03-24 15:22:37 -0700

    LinkedList

--- linkedList.py
+++ linkedList.py
@@ -1,33 +1,70 @@
+import math
+
 class Node:
     def __init__(self,DATA):
         self.DATA = DATA
         self.next = None
 
+class Linked_L:
 
-class LinkedL:
-    def __init__(self,head=None):
+    def __init__(self,head = None):
         self.head = head
-
-
-    def Append(self,DATA):
+        self.counter = 0
+    
+    def append(self,DATA):
         New_node = Node(DATA)
-        if self.head is None:
+        if self.head is None :
             self.head = New_node
+            self.counter += 1
         else:
-            current = self.head
-            current.next = current
-            while current.next:
-                current = current.next
+            curr = self.head
+            while curr.next is not None:
+                curr = curr.next
+                self.counter += 1
+            curr.next = New_node
+
     
     def dis(self):
         curr = self.head
-        if curr:
-            while curr:
-                print(curr.DATA)
-                curr = curr.next
+        while curr:
+            print(curr.DATA)
+            curr = curr.next
 
-my = LinkedL()
-my.Append(1)
-my.Append(2)
-my.Append(3)
-my.dis()
+
+    def disH(self):
+        curr = self.head
+        half_counter = 0
+        while curr:
+            half_counter+=1
+            if half_counter == math.floor(self.counter/2):
+                print(curr.DATA)
+            curr = curr.next
+ 
+
+    def Remove_by_index(self, index):
+            curr = self.head
+            prev = None
+            counter = 0
+            
+            while curr:
+                if counter == index:
+                    if prev:  
+                        prev.next = curr.next
+                    
+                prev = curr
+                curr = curr.next
+                counter += 1
+
+
+
+L = Linked_L()
+
+L.append(1)
+L.append(2)
+L.append(3)
+L.append(4)
+L.append(5)
+L.dis()
+print('####################################')
+L.Remove_by_index(2)
+L.dis()
